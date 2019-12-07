2cab03d0e7ea7983ac5e98d5c0041166a6eaa8fa
Luana Pasquet <pasquet.luana@gmail.com>
2019-12-07 19:11:17 -0300

    URI Online Judge | 1001 - Extremely Basic Added README file.
    Signed-off-by: Luana Pasquet <pasquet.luana@gmail.com>

---
+++ src/urijudge/Uri_1001.java
@@ -0,0 +1,20 @@
+package urijudge;
+import java.util.Scanner;
+
+public class Uri_1001 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a, b, c;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		c = a + b;
+		System.out.println("X = " + c);
+
+		sc.close();
+
+	}
+
+}
